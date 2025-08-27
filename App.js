b9d4c35a0e28ab4d74837eb6e401102429db6c77
RekoLep <reko.leppanen@myy.haaga-helia.fi>
2025-08-27 13:11:44 +0300

    lisäys

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+const [text, setText] = useState("");
 
 export default function App() {
+  const buttonPressed = () => {
+    console.log("Button pressed!");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      flex: 1,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-around'
+    }}>
+      <TextInput
+        placeholder='Enter some text'
+        onChangeText={text => setText(text)}
+        value={text}
+      />
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Button title="+" onPress={buttonPressed} />
+      </View>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Button title="-" onPress={buttonPressed} />
+      </View>
     </View>
   );
 }
